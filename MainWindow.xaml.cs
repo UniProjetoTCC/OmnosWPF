a5d94a041d055dc31bda47ba7a77e2d2e79b1202
Felipe R. Martins <dev.feliperochamartins@gmail.com>
2025-05-16 14:41:30 -0300

    Initial Project Setup

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OmnosWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window    
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToNextScreen(object sender, RoutedEventArgs e)
+        {
+            Window1 newWindow = new Window1();
+            newWindow.Show();
+            this.Close(); // ou use Hide() se quiser manter a janela aberta
+        }
+    }
+}
