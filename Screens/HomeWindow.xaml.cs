47b88808cf15df1ab81cf9aedb4a711fb6998e44
Adriel Campos <144078275+AdrielCampos134@users.noreply.github.com>
2025-05-20 20:12:37 -0300

    Adicionar arquivos de projeto.

---
+++ Screens/HomeWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LoginApp
+{
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Screens/LoginWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LoginApp
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        // Evento para clicar e arrastar a janela pela barra superior customizada
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        // Evento do botão "X" para fechar a janela
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Clique em "Forgot Password?"
+        private void ForgotPassword_Click(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Funcionalidade de recuperação de senha ainda não implementada.");
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+            bool rememberMe = RememberMeCheck.IsChecked == true;
+
+            if (username == "admin" && password == "123")
+            {
+                MessageBox.Show("Login bem-sucedido!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Abrir a janela de início
+                HomeWindow home = new HomeWindow();
+                home.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha inválidos.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+    }
+}
